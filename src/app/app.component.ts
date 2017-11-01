5796e8d773aa6adc39b961ebc006bb904e554402
jangya <jangya069@gmail.com>
2017-11-02 00:19:31 +0530

    discount logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,10 @@ export class AppComponent implements OnInit{
   cartLength:number;
   private totalPrice:number;
   private totalQuantity:number;
+  promoCode:number;
   constructor(private appService: AppService) {
     //this.totalPrice= this.getTotal(this.cartData);
-    //this.cartData.forEach(s => this.totalQuantity += s.p_quantity );
+    // this.cartData.forEach(s => this.totalQuantity += s.p_quantity );
    }
   ngOnInit() {
     this.appService.getData();
@@ -24,13 +25,36 @@ export class AppComponent implements OnInit{
     });
     
   }
-  getTotalPrice() {
-    //return this.cartData.forEach(s => this.totalPrice += (s.p_price*s.p_quantity) )
+  getTotal() {
+    let totalPrice = 0;
+    let totalQuantity = 0;
     if(typeof this.cartData !== 'undefined') {
-      return this.cartData.reduce((total, item) => {
-          return total + (item.p_price*item.p_quantity);
-      }, 0); 
+      this.cartData.forEach(d => {
+        totalQuantity += d.p_quantity;
+        totalPrice += (d.p_price*d.p_quantity);
+      });
+      //console.log("Quantity",totalQuantity);
+      if(totalQuantity === 3){
+        this.promoCode = 5;
+      }else if(totalQuantity > 3 && totalQuantity <10){
+        this.promoCode = 10;
+      }else if(totalQuantity > 10){
+        this.promoCode = 25;
+      }else{
+        this.promoCode = 0;
+      }
     }
-    return 0;
+  
+    return {
+      price: totalPrice,
+      quantity: totalQuantity
+    };
+    //return this.cartData.forEach(s => this.totalPrice += (s.p_price*s.p_quantity) )
+    // if(typeof this.cartData !== 'undefined') {
+    //   return this.cartData.reduce((total, item) => {
+    //       return total + (item.p_price*item.p_quantity);
+    //   }, 0); 
+    // }
+    // return 0;
   }
 }
